cad93a41285c122c67de331f1eb8089cb3d0c336
Davila Yoimer <89022117+Yoimer-Davila@users.noreply.github.com>
2025-12-11 09:44:45 -0500

    chore: remove unused compose dependencies

--- library/build.gradle.kts
+++ library/build.gradle.kts
@@ -42,9 +42,7 @@ kotlin {
         commonMain.dependencies {
             implementation(compose.runtime)
             implementation(compose.foundation)
-            implementation(compose.material3)
             implementation(compose.ui)
-            implementation(compose.components.resources)
         }
 
         commonTest.dependencies {
