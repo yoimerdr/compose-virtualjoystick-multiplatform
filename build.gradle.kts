376e96875af12a7d6db4f10d5492de0a7130b9bf
Davila Yoimer <89022117+Yoimer-Davila@users.noreply.github.com>
2025-12-11 09:44:55 -0500

    refactor: update Gradle plugin application for Android modules

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,6 @@
 plugins {
+    alias(libs.plugins.androidApplication) apply false
+    alias(libs.plugins.androidLibrary) apply false
     alias(libs.plugins.android.kotlin.multiplatform.library) apply false
     alias(libs.plugins.kotlinMultiplatform) apply  false
     alias(libs.plugins.vanniktech.mavenPublish) apply false
