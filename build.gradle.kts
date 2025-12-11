08d26254352ed68c18233e35085dea98f1f10810
Davila Yoimer <89022117+Yoimer-Davila@users.noreply.github.com>
2025-12-11 12:02:31 -0500

    chore: add composeHotReload plugin to Gradle configuration

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,4 +6,5 @@ plugins {
     alias(libs.plugins.vanniktech.mavenPublish) apply false
     alias(libs.plugins.composeMultiplatform) apply false
     alias(libs.plugins.composeCompiler) apply false
+    alias(libs.plugins.composeHotReload) apply false
 }
